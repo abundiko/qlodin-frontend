c50737114a80417b3955f7839f76c3cc85638233
abundiko <abundiko111@gmail.com>
2025-01-08 14:05:16 +0100

    modified google auth button, hook and fixed google register action

--- components/hoc/FirebaseWrapper.tsx
+++ components/hoc/FirebaseWrapper.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import {getFirebaseApp} from "@/lib/firebase"
 
 
 export default function FirebaseWrapper({ children }: { children: ReactNode }) {
+  const [app, setApp] = useState<any>(null);
+  
   useEffect(() => {
-    getFirebaseApp();
-  }, []);
+    if(app) return;
+    const _app = getFirebaseApp();
+    if(_app) setApp(_app);
+  });
 
+  if(app)
   return children;
 }
--- hooks/useSocialAuth.google.ts
+++ hooks/useSocialAuth.google.ts
@@ -6,10 +6,10 @@ import {
   User,
   getAuth,
   getRedirectResult,
-  signInWithRedirect
+  signInWithRedirect,
 } from "firebase/auth";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import toast from "react-hot-toast";
 
 interface GoogleAuthHook {
@@ -21,6 +21,68 @@ export const useGoogleAuth = (): GoogleAuthHook => {
   const [loading, setLoading] = useState<boolean>(false);
   const { replace, push } = useRouter();
 
+  useEffect(() => {
+    async function listenForAuth() {
+      try {
+        setLoading(true);
+        // alert("hmm")
+        // return;
+
+        const auth = getAuth();
+        // const provider = new GoogleAuthProvider();
+        
+        const result = await getRedirectResult(auth);
+        debugLog({ auth, result });
+        if (!result) {
+          return;
+        }
+        let user: User = result.user;
+
+        // check if the user is already registered
+        const res = await googleLoginAction(user.email!, user.uid);
+        if (res === "success") {
+          toast.success("Google login successful");
+          replace(__paths.user);
+          return;
+        }
+        if (res === 404) {
+          debugLog("hmmmm");
+          await googleRegisterAction({
+            email: user.email!,
+            firstName: user.displayName?.split(" ")[0] ?? "",
+            lastName: user.displayName?.split(" ")[1] ?? "",
+            googleId: user.uid,
+          });
+          push(__paths.signUpGoogle);
+          return;
+        }
+        toast.error(res);
+
+        // Log user details
+        // console.log("User details:", {
+        //   uid: user.uid,
+        //   name: user.displayName,
+        //   email: user.email,
+        //   photoURL: user.photoURL,
+        // });
+
+        // Additional tasks like sending user details to a backend can be added here
+      } catch (error: any) {
+        debugLog({ error, "@": "hooks/useSocialAuth.google.ts:58" });
+        if (error.toString().includes("auth/popup-blocked"))
+          toast.error("Google Authentication Pop-up Blocked");
+        else if (error.toString().includes("auth/popup-closed-by-user"))
+          toast.error("Google Authentication Cancelled");
+        else toast.error("Google Authentication Failed");
+        debugLog("Error during Google Sign-In:", error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    listenForAuth();
+  }, []);
+
   const startAuth = async (): Promise<void> => {
     const auth = getAuth();
     const provider = new GoogleAuthProvider();
@@ -30,7 +92,7 @@ export const useGoogleAuth = (): GoogleAuthHook => {
 
       // Initiate Google Sign-In
       await signInWithRedirect(auth, provider);
-      const result = await getRedirectResult(auth, provider);
+      const result = await getRedirectResult(auth);
       if (!result) {
         toast.error("Google authentication error");
         return;
